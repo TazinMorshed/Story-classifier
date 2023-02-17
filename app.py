01d47666dd03e37e1dd1f72aab812cf40cc1f313
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-02-17 18:53:34 +0600

    implement backend

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        output = predict_genres(request.form['text'])[0]
+
+        confidence_list = output['confidences']
+        labels = [elem['label'] for elem in confidence_list if elem['confidence'] >= 0.8 ]
+        label_text = ""
+        for idx, label in enumerate(labels):
+            label_text = label_text + label
+            if idx != len(labels)-1: label_text = label_text + ", "
+
+        print(label_text)
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
+
+
+def predict_genres(input_text):
+    response = requests.post("https://tazinmorshed-story-classifier.hf.space/run/predict", json={
+        "data": [
+            input_text,
+        ]
+    }).json()
+
+    data = response["data"]
+
+    return data
+
+if __name__=="__main__":
+    app.run(debug=True)
