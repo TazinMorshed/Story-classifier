accbb7ca9dee24fd16f325cd819cb8e776f3e3ca
Tazin <45015333+TazinMorshed@users.noreply.github.com>
2023-02-17 19:05:27 +0600

    added styles and webpage

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
 from flask import Flask, render_template, request
 import requests
 
-
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        output = predict_genres(request.form['text'])[0]
-
+    if request.method == "POST":
+        input_text = request.form['text']
+        output = predict_genres(input_text)[0]
         confidence_list = output['confidences']
         labels = [elem['label'] for elem in confidence_list if elem['confidence'] >= 0.8 ]
         label_text = ""
         for idx, label in enumerate(labels):
             label_text = label_text + label
             if idx != len(labels)-1: label_text = label_text + ", "
-
-        print(label_text)
-        return render_template('index.html')
+        return render_template("result.html", input_text=input_text, output_text=label_text)
     else:
-        return render_template('index.html')
+        return render_template("index.html")
+    
 
 
 def predict_genres(input_text):
